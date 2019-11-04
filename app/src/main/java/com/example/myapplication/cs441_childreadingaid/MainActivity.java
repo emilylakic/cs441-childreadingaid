49303580e9911cd638d85f5e8d1910493e7168cc
emilylakic <emilylakic@gmail.com>
2019-11-04 10:59:46 -0500

    Home button added

--- app/src/main/java/com/example/myapplication/cs441_childreadingaid/MainActivity.java
+++ app/src/main/java/com/example/myapplication/cs441_childreadingaid/MainActivity.java
@@ -5,19 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     Button letsBegin;
+    ImageButton aboutButton;
+    ImageButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         letsBegin = (Button) findViewById(R.id.letsBegin);
+        aboutButton = (ImageButton) findViewById(R.id.aboutButton);
+        homeButton = (ImageButton) findViewById(R.id.homeButton);
         letsBegin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SixthActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        homeButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
